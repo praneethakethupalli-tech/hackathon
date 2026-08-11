01bb68be9cd9acf8d7a1d980caadba9b1bd64252
praneethakethupalli-tech <praneethakethupalli@gmail.com>
2026-08-11 15:46:35 +0530

    Delete hackathon directory

--- hackathon/components/core/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './text-effect';
-export * from './text-shimmer';
-export * from './animated-background';
-export * from './morphing-dialog';
-export * from './infinite-slider';
-export * from './glow-effect';
-export * from './disclosure';
